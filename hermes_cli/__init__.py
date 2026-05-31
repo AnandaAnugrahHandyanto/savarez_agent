172ab0ee4da74e12bcf01caf19c22123765c8e09
adybag14-cyber <adybag14@gmail.com>
2026-06-01 00:40:30 +0100

    Release Hermes Agent Fork v0.13.127

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.13.126"
-__release_date__ = "2026.5.29"
+__version__ = "0.13.127"
+__release_date__ = "2026.5.31"
 
 
 def _ensure_utf8():
