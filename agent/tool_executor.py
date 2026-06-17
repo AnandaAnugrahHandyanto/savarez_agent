burgebj/hermes

fix: remediate all 7 GPT-5.5 review findings for interactive_prompt tool
P1: Fix double-JSON-encoding — detect and pass through already-serialized
JSON strings from production gateway callbacks instead of re-encoding.

P1: Add text-fallback resolution for non-rich platforms — add
get_pending_for_session/mark_awaiting_text/resolve_text_response to
human_input_gateway, wire base-platform send_human_input to mark
text-capture, add gateway intercept for interactive_prompt text replies.

P2: Use config-aware timeout in AIAgent intercepted paths — both
agent_runtime_helpers.py and tool_executor.py now call
get_interactive_prompt_timeout() when timeout_seconds is absent.

P2: Wrap interactive_prompt in _finish_agent_tool in runtime_helpers so
post-tool-call hooks fire (timing, metrics, tracing).

P2: Use get_hermes_home() instead of hardcoded ~/.hermes for upload cache
path in discord_interactive_views.py.

P2: Implement per-policy auth checks — any_allowed_user only checks user
allowlist, any_allowed_role only checks role allowlist,
any_allowed_user_or_role checks union.

P3: Enforce runtime validation — reject invalid display_type and
auth_policy values, clamp timeout_seconds to 60-3600 range.

45 new tests covering all 7 findings.

agent/tool_executor.py
@@ -940,11 +940,14 @@ def execute_tool_calls_sequential(agent, assistant_message, messages: list, effe
                 agent._vprint(f"  {_get_cute_tool_message_impl('clarify', function_args, tool_duration, result=function_result)}")
         elif function_name == "interactive_prompt":
             from tools.interactive_prompt_tool import interactive_prompt_tool as _ip_tool
+            from tools.human_input_gateway import get_interactive_prompt_timeout
+            _raw_timeout = function_args.get("timeout_seconds")
+            _timeout = int(_raw_timeout) if _raw_timeout is not None else get_interactive_prompt_timeout()
             function_result = _ip_tool(
                 question=function_args.get("question", ""),
                 options=function_args.get("options", []),
                 display_type=function_args.get("display_type", "buttons"),
-                timeout_seconds=float(function_args.get("timeout_seconds", 900)),
+                timeout_seconds=_timeout,
                 auth_policy=function_args.get("auth_policy", "session_owner_only"),
                 callback=getattr(agent, "interactive_prompt_callback", None),
             )

AnandaAnugrahHandyanto/savarez_agent

agent/tool_executor.py
@@ -1087,11 +1087,14 @@ def execute_tool_calls_sequential(agent, assistant_message, messages: list, effe
         elif function_name == "interactive_prompt":
             def _execute(next_args: dict) -> Any:
                 from tools.interactive_prompt_tool import interactive_prompt_tool as _ip_tool
+                from tools.human_input_gateway import get_interactive_prompt_timeout
+                _raw_timeout = next_args.get("timeout_seconds")
+                _timeout = int(_raw_timeout) if _raw_timeout is not None else get_interactive_prompt_timeout()
                 return _ip_tool(
                     question=next_args.get("question", ""),
                     options=next_args.get("options", []),
                     display_type=next_args.get("display_type", "buttons"),
-                    timeout_seconds=float(next_args.get("timeout_seconds", 900)),
+                    timeout_seconds=_timeout,
                     auth_policy=next_args.get("auth_policy", "session_owner_only"),
                     callback=getattr(agent, "interactive_prompt_callback", None),
                 )